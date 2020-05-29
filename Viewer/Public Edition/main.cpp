ac1d2a25925afc8474e4dbe09cae1cc798a2e03d
JMJAJ <34998292+JMJAJ@users.noreply.github.com>
2020-05-29 12:24:38 +0200

    Add files via upload

--- Viewer/Public Edition/main.cpp
+++ Viewer/Public Edition/main.cpp
@@ -9,14 +9,20 @@ using namespace std;
 
 int main()
 {
+    cout << "You have file in same folder with this application" << endl;
+    cout << "Example - You have Hello.txt on Desktop, so you must have the application in the same folder - on Desktop" << endl;
+    cout << "Example - Desktop is also a folder - C:/Users/<YourName>/Desktop/\n" << endl;
+
+    system("pause");
     system("cls");
 
     cout << "You must write <FileName>.txt <- .txt for good working\n" << endl;
-    cout << "Support types of document\n\n - .txt\n - .dat\n - .c/cpp\n - .html/css/java\n - coming soon\n\n" << endl;
-    cout << "Not support types of document\n\n - .docx\n - coming soon\n\n" << endl;
+    cout << "Supported types of document\n\n - .txt\n - .dat\n - .c/cpp\n - .html/css/java\n - coming soon\n\n" << endl;
+    cout << "Not supported types of document\n\n - .docx\n - coming soon\n\n" << endl;
     cout << "Example: try.txt\n" << endl;
 
     string fileName;
+    string path; // example path - C:/Users/Brudy/Desktop/54511
     getline(cin, fileName);
     //cout << fileName << endl;
 
